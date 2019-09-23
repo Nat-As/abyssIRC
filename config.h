f7b75d83a65015eca3d0178d59840d8cb494fa3a
James Andrews <jandrews7348@floridapoly.edu>
2019-09-22 23:35:07 -0400

    Port 6668

--- config.h
+++ config.h
@@ -2,6 +2,6 @@
 #include <iostream>
 
 extern std::string server = "irc.freenode.net"; // network address
-extern int port = 6667; // server port
+extern int port = 6668; // server port
 extern std::string nick = "NICK Somebot"; // NICK raw
 extern std::string user = "USER Somebot randomtext israndom :My first c++ bot"; // USER raw
