be7274351e8b06904ec00797e62c486e4500a8e3
James Andrews <jandrews7348@floridapoly.edu>
2019-10-14 13:14:43 -0400

    Delete config.h

--- config.h
+++
@@ -1,7 +0,0 @@
-#include <string>
-#include <iostream>
-
-extern std::string server = "irc.freenode.net"; // network address
-extern int port = 6668; // server port
-extern std::string nick = "NICK Somebot"; // NICK raw
-extern std::string user = "USER Somebot randomtext israndom :My first c++ bot"; // USER raw
