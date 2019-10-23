a366596df2028bf7a38c80f59e82355f0b357d59
James Andrews <jandrews7348@floridapoly.edu>
2019-10-23 18:39:05 -0400

    server loop

--- main.cpp
+++ main.cpp
@@ -1,8 +1,60 @@
-#include <sys/wait.h>
-#include <cstdlib>
-#include <unistd.h>
+#include <sys/wait.h> // For Kernel interaction
+#include <sys/socket.h> // For socket functions
+#include <netinet/in.h> // For sockaddr_in
+#include <cstdlib> // For exit() and EXIT_FAILURE
+#include <iostream> // For cout
+#include <unistd.h> // For read
 
 using namespace std;
+
+int sloop(){
+
+    // Create a simple socket (IPv4, TCP)
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == 0) {
+        std::cout << "Failed to create socket. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen to port 6668 on any address
+    sockaddr_in sockaddr;
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_addr.s_addr = INADDR_ANY;
+    sockaddr.sin_port = htons(6668); // htons is necessary to convert a number to
+
+    // error handling
+    if (bind(sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
+        std::cout << "Failed to bind to port 6668. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Start listening. Hold at most 10 connections in the queue
+    if (listen(sockfd, 10) < 0) {
+        std::cout << "Failed to listen on socket. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Grab a connection from the queue
+    auto addrlen = sizeof(sockaddr);
+    int connection = accept(sockfd, (struct sockaddr*)&sockaddr, (socklen_t*)&addrlen);
+    if (connection < 0) {
+        std::cout << "Failed to grab connection. errno: " << errno << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Read from the connection
+    char buffer[100];
+    auto bytesRead = read(connection, buffer, 100);
+    std::cout << "The message was: " << buffer;
+
+    // Send a message to the connection
+    std::string response = "Good talking to you\n";
+    send(connection, response.c_str(), response.size(), 0);
+
+    // Close the connections
+    close(connection);
+    close(sockfd);
+}
 
 int main(void)
 {
